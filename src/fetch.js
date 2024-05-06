6bef7afaea7c6a6c4216278fb65329d83e344843
victor kamau <vikakamau04@gmail.com>
2024-05-06 11:26:00 +0300

    final commit

--- src/fetch.js
+++ src/fetch.js
@@ -1,14 +1,14 @@
 export function getBots(){
-    return fetch("http://localhost:3000/bots")
+    return fetch("https://code-challenge-2-deploy-1.onrender.com/bots")
     .then(res=> res.json())
 }
  
 export function getBotById(id){
-    return fetch(`http://localhost:3000/bots/${id}`)
+    return fetch(`https://code-challenge-2-deploy-1.onrender.com/bots/${id}`)
     .then(res=> res.json())
 }
 export function deleteBotById(id) {
-    return fetch(`http://localhost:3000/bots/${id}`, {
+    return fetch(`https://code-challenge-2-deploy-1.onrender.com/bots/${id}`, {
         method: 'DELETE'
     })
     .then(response => response.json())
